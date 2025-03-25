bb92077f9e9b7b4f7171fcf3714947183b6e636c
Bruno de Souza da Silva <bruno7317@gmail.com>
2025-03-24 19:10:51 -0600

    Opens access to the Static website

--- infra/main.tf
+++ infra/main.tf
@@ -17,6 +17,15 @@ resource "aws_s3_bucket" "cloudweave_s3_bucket" {
   force_destroy = true
 }
 
+resource "aws_s3_bucket_public_access_block" "cloudweave_bucket_public_access" {
+  bucket = aws_s3_bucket.cloudweave_s3_bucket.bucket
+
+  block_public_acls = false
+  block_public_policy = false
+  ignore_public_acls = false
+  restrict_public_buckets = false
+}
+
 resource "aws_s3_bucket_website_configuration" "cloudweave_s3_website_configuration" {
   bucket = aws_s3_bucket.cloudweave_s3_bucket.bucket
 

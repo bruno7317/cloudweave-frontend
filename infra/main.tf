53d69caf6e7bac5dc4950304c8e1bdf8b2936f1d
Bruno de Souza da Silva <bruno7317@gmail.com>
2025-03-24 19:25:12 -0600

    Bucket policy

--- infra/main.tf
+++ infra/main.tf
@@ -26,6 +26,22 @@ resource "aws_s3_bucket_public_access_block" "cloudweave_bucket_public_access" {
   restrict_public_buckets = false
 }
 
+resource "aws_s3_bucket_policy" "allow_all_access" {
+  bucket = aws_s3_bucket.cloudweave_s3_bucket.bucket
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [{
+      Effect = "Allow"
+      Principal = "*"
+      Action = [
+        "s3:GetObject"
+      ]
+      Resource = "${aws_s3_bucket.cloudweave_s3_bucket.arn}/*"
+    }]
+  })
+  
+}
+
 resource "aws_s3_bucket_website_configuration" "cloudweave_s3_website_configuration" {
   bucket = aws_s3_bucket.cloudweave_s3_bucket.bucket
 
